160ca5f9ea33709e6f55475c4e0057070c2c5d2f
Amit Gupta <amitkrgupta25@gmail.com>
2024-01-14 20:24:47 +0530

    Created first react file wih Vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1>
+      Chai Aur React | Amit Gupta | Today I learned about how to install React
+      with Vite
+    </h1>
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai Aur Code with Vite | Amit Gupta</h1>;
+}
+
+export default App;
